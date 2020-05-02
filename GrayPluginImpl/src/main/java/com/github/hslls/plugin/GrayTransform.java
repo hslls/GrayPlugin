424644b9452abc3edef42afceb9b8008405d54a4
hslls <lifengang@gmail.com>
2020-05-03 01:51:52 +0800

    no message

--- GrayPluginImpl/src/main/java/com/github/hslls/plugin/GrayTransform.java
+++ GrayPluginImpl/src/main/java/com/github/hslls/plugin/GrayTransform.java
@@ -11,12 +11,15 @@ import java.io.File;
 import java.util.Set;
 
 import javassist.CannotCompileException;
+import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.CtNewMethod;
-import javassist.Modifier;
 import javassist.NotFoundException;
 
+/**
+ * @author <a href="mailto:249418416@qq.com">mapleleaf</a>
+ */
 public class GrayTransform extends EasyTransform {
 
     public GrayTransform(Project project) {
@@ -36,61 +39,193 @@ public class GrayTransform extends EasyTransform {
     @Override
     public Set<? super QualifiedContent.Scope> getScopes() {
 //        return TransformManager.SCOPE_FULL_PROJECT;
-        return ImmutableSet.of(QualifiedContent.Scope.PROJECT, QualifiedContent.Scope.SUB_PROJECTS);
+        return ImmutableSet.of(QualifiedContent.Scope.EXTERNAL_LIBRARIES,
+                QualifiedContent.Scope.PROJECT, QualifiedContent.Scope.SUB_PROJECTS);
     }
 
     @Override
     protected boolean isJarFileNeedModify(File jarFile) {
-        return true;
+        String jarFilePath = jarFile.getAbsolutePath();
+        if (jarFilePath.contains("appcompat")) {
+            try {
+                ClassPool.getDefault().appendClassPath(jarFilePath);
+            } catch (NotFoundException e) {
+
+            }
+        }
+        return !jarFilePath.contains("gradle/caches/transforms");
     }
 
     @Override
     protected boolean justModifyNotWriteBack(CtClass ctClass) {
-        if (ctClass.isInterface() ||
-                ((ctClass.getModifiers() & Modifier.ABSTRACT) == Modifier.ABSTRACT)) {
+        if (ctClass.isInterface()) {
             return false;
         }
 
-        boolean hasModified = false;
-        CtMethod[] methods = ctClass.getMethods();
-        for (CtMethod m : methods) {
-            String methodName = m.getName();
-            if (methodName.equals("onCreate")) {
-                CtClass[] types;
-                try {
-                    types = m.getParameterTypes();
-                } catch (NotFoundException e) {
-                    break;
-                }
-                boolean insert = ((types.length == 1) && types[0].getName().equals("android.os.Bundle"));
-                // ((types.length == 2) && types[0].getName().equals("android.os.Bundle") && types[1].getName().equals("android.os.PersistableBundle"));
-                if (!insert) {
-                    break;
-                }
+        FindMethodResult findOnCreateResult = findMethod(ctClass, "onCreate",
+                "(Landroid/os/Bundle;)V");
+        if (findOnCreateResult.notFound()) {
+            return false;
+        }
 
-                if (!ctClass.getName().equals(m.getDeclaringClass().getName())) {
-                    try {
-                        m = CtNewMethod.delegator(m, ctClass);
-                        ctClass.addMethod(m);
-                    } catch (CannotCompileException e) {
-                        break;
-                    }
-                }
+        FindMethodResult findGetWindowResult = findMethod(ctClass, "getWindow",
+                "()Landroid/view/Window;");
+        FindMethodResult findFindViewByIdResult = findMethod(ctClass, "findViewById",
+                "(I)Landroid/view/View;");
+        if (findGetWindowResult.notFound() && findFindViewByIdResult.notFound()) {
+            return false;
+        }
 
-                try {
-                    m.insertAfter(
-                            "android.graphics.Paint paint = new android.graphics.Paint();" +
-                                    "android.graphics.ColorMatrix cm = new android.graphics.ColorMatrix();" +
-                                    "cm.setSaturation(0f);" +
-                                    "paint.setColorFilter(new android.graphics.ColorMatrixColorFilter(cm));" +
-                                    "getWindow().getDecorView().setLayerType(android.view.View.LAYER_TYPE_SOFTWARE, paint);");
-                } catch (CannotCompileException e) {
-                    break;
-                }
-                hasModified = true;
+        if (findOnCreateResult.mStatus == FindMethodStatus.IN_SUPER_CLASS) {
+            if (!addOverrideMethod(ctClass, findOnCreateResult.mFindMethod)) {
+                return false;
             }
         }
 
-        return hasModified;
+        boolean hasGetWindowMethod = false;
+        if (findGetWindowResult.mStatus == FindMethodStatus.IN_THIS_CLASS) {
+            hasGetWindowMethod = true;
+        } else if (findGetWindowResult.mStatus == FindMethodStatus.IN_SUPER_CLASS) {
+            if (addOverrideMethod(ctClass, findGetWindowResult.mFindMethod)) {
+                hasGetWindowMethod = true;
+            }
+        }
+        if (hasGetWindowMethod) {
+            if (!addGetDecorViewMethod1(ctClass)) {
+                return false;
+            }
+        } else {
+            boolean hasFindViewByIdMethod = false;
+            if (findFindViewByIdResult.mStatus == FindMethodStatus.IN_THIS_CLASS) {
+                hasFindViewByIdMethod = true;
+            } else if (findFindViewByIdResult.mStatus == FindMethodStatus.IN_SUPER_CLASS) {
+                if (addOverrideMethod(ctClass, findFindViewByIdResult.mFindMethod)) {
+                    hasFindViewByIdMethod = true;
+                }
+            }
+            if (hasFindViewByIdMethod) {
+                if (!addGetDecorViewMethod2(ctClass)) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if (!addGetTurnGrayMethod(ctClass)) {
+            return false;
+        }
+
+        try {
+            findOnCreateResult.mFindMethod.insertAfter("turnGray(getDecorView());");
+        } catch (CannotCompileException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private enum FindMethodStatus {
+
+        NOT_FOUND,
+        IN_THIS_CLASS,
+        IN_SUPER_CLASS
+
+    }
+
+    private static class FindMethodResult {
+
+        private FindMethodStatus mStatus = FindMethodStatus.NOT_FOUND;
+        private CtMethod mFindMethod;
+
+        private boolean notFound() {
+            return ((mStatus == FindMethodStatus.NOT_FOUND) || (mFindMethod == null));
+        }
+    }
+
+    private static FindMethodResult findMethod(CtClass ctClass, String methodName, String methodDesc) {
+        FindMethodResult result = new FindMethodResult();
+        try {
+            result.mFindMethod = ctClass.getMethod(methodName, methodDesc);
+            CtClass ctDeclaringClass = result.mFindMethod.getDeclaringClass();
+            if (ctDeclaringClass != null) {
+                result.mStatus = (ctClass.getName().equals(ctDeclaringClass.getName())) ?
+                        FindMethodStatus.IN_THIS_CLASS : FindMethodStatus.IN_SUPER_CLASS;
+            }
+        } catch (NotFoundException e) {
+
+        }
+        return result;
+    }
+
+    private static boolean addOverrideMethod(CtClass ctClass, CtMethod method) {
+        try {
+            CtMethod methodOverride = CtNewMethod.delegator(method, ctClass);
+            ctClass.addMethod(methodOverride);
+        } catch (CannotCompileException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean addGetDecorViewMethod1(CtClass ctClass) {
+        CtMethod method;
+        try {
+            method = CtNewMethod.make(
+                    "private android.view.View getDecorView() {" +
+                            "return getWindow().getDecorView();" +
+                            "}",
+                    ctClass);
+            ctClass.addMethod(method);
+        } catch (CannotCompileException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean addGetDecorViewMethod2(CtClass ctClass) {
+        CtMethod method;
+        try {
+            method = CtNewMethod.make(
+                    "private android.view.View getDecorView() {" +
+                            "android.view.View cv = findViewById(android.R.id.content);" +
+                            "android.view.View dv = cv;" +
+                            "if (cv != null) {" +
+                            "android.view.ViewParent vp = cv.getParent();" +
+                            "while (vp instanceof android.view.View) {" +
+                            "dv = (android.view.View) vp;" +
+                            "vp = vp.getParent();" +
+                            "}" +
+                            "}" +
+                            "return dv;" +
+                            "}",
+                    ctClass);
+            ctClass.addMethod(method);
+        } catch (CannotCompileException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean addGetTurnGrayMethod(CtClass ctClass) {
+        CtMethod method;
+        try {
+            method = CtNewMethod.make(
+                    "private void turnGray(android.view.View decorView) {" +
+                            "if (decorView == null) {" +
+                            "return;" +
+                            "}" +
+                            "android.graphics.Paint paint = new android.graphics.Paint();" +
+                            "android.graphics.ColorMatrix cm = new android.graphics.ColorMatrix();" +
+                            "cm.setSaturation(0f);" +
+                            "paint.setColorFilter(new android.graphics.ColorMatrixColorFilter(cm));" +
+                            "decorView.setLayerType(android.view.View.LAYER_TYPE_SOFTWARE, paint);" +
+                            "}",
+                    ctClass);
+            ctClass.addMethod(method);
+        } catch (CannotCompileException e) {
+            return false;
+        }
+        return true;
     }
 }
